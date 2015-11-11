1d1e71571339dc93f54d85bd7ba64752dcf0c4cd
Nsity <nastyafed93@mail.ru>
2015-11-11 23:26:02 +0300

    Auth

--- application/models/model.php
+++ application/models/model.php
@@ -8,4 +8,13 @@
 
 		}
 
+
+		function getPupil($login, $hash) {
+			$query = $this->db->query("SELECT p.PUPIL_LOGIN, p.PUPIL_HASH, p.ROLE_ID, p.PUPIL_STATUS, p.PUPIL_ID, pc.CLASS_ID
+			FROM PUPIL p JOIN PUPILS_CLASS pc ON p.PUPIL_ID = pc.PUPIL_ID JOIN CLASS c ON c.CLASS_ID = pc.CLASS_ID
+			WHERE PUPIL_LOGIN collate utf8_bin = '$login' AND PUPIL_HASH = '$hash' AND CLASS_STATUS = 1");
+			return $query->row_array();
+		}
+
+
 	}
